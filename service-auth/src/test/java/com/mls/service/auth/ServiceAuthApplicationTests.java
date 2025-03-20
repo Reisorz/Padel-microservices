ac3a4556e68570d605476d410285c7fe8f52b794
Reisorz <miguelminib@gmail.com>
2025-03-20 16:52:21 +0100

    ADD Dockerfiles and docker-compose configuration for msvc-gateway & msvc-auth.

--- service-auth/src/test/java/com/mls/service/auth/ServiceAuthApplicationTests.java
+++ service-auth/src/test/java/com/mls/service/auth/ServiceAuthApplicationTests.java
@@ -3,7 +3,7 @@ package com.mls.service.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ServiceAuthApplicationTests.class})
 class ServiceAuthApplicationTests {
 
 	@Test
